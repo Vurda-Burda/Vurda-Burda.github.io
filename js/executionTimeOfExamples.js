8ec4afe126df7d8c09a739d06e2a23e2c99d7cb6
Aleksandr Bolokhov <bolohov@inbox.ru>
2023-12-25 13:51:24 +0300

    update timer

--- js/executionTimeOfExamples.js
+++ js/executionTimeOfExamples.js
@@ -1,23 +1,21 @@
+function timer() {
 let workTime = 0;
 let minute = 0;
-
 document.querySelector('.WorkTime').innerHTML = 'Время выполнения: ' + minute + ' мин ' + workTime + ' сек';
 let timer = setInterval(
    () => {
       if(minute == 60) {
          clearInterval(timer);
-      } 
+      };
       workTime++;
       if(workTime == 60) {
          minute ++;
          workTime = 0;
-      }
+      };
       document.querySelector('.WorkTime').innerHTML = 'Время выполнения: ' + minute + ' мин ' + workTime + ' сек';
    },
    1000
 );
+};
 
-
-
-
-
+timer();
